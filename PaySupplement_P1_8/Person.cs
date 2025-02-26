24b62fefa22e8981d961dc87ce50c2261b37fc39
donp8on <paytonadonis@gmail.com>
2025-02-25 21:03:57 -0500

    created person class with Id, Name, and Birthday

---
+++ PaySupplement_P1_8/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PaySupplement_P1_8
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
