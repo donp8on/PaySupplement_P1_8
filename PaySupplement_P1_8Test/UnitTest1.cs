bb1fc680f8fb3de3732ed871fad8e546e7b89d88
donp8on <paytonadonis@gmail.com>
2025-02-25 21:23:48 -0500

    test: created test for GetPeopleBornAfter method

--- PaySupplement_P1_8Test/UnitTest1.cs
+++ PaySupplement_P1_8Test/UnitTest1.cs
@@ -11,5 +11,15 @@ namespace PaySupplement_P1_8Test
             _getPeople = new GetPeople();
             _getPeople.InitializePeople();
         }
+
+        [Fact]
+        public void ShouldGetPeopleBornAfter_CorrectResults()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            var results = _getPeople.GetPeopleBornAfter(testDate);
+
+            Assert.NotEmpty(results);
+            Assert.All(results, p => Assert.True(p.Birthday > testDate));
+        }
     }
 }
