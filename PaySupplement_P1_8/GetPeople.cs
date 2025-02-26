c62cb9e7d7ff8bc385d78b2266fa05afd0d17805
donp8on <paytonadonis@gmail.com>
2025-02-25 21:06:30 -0500

    chore: declared methods for GetPeople class

--- PaySupplement_P1_8/GetPeople.cs
+++ PaySupplement_P1_8/GetPeople.cs
@@ -2,6 +2,32 @@
 {
     public class GetPeople
     {
+        private List<Person> data = new List<Person>();
 
+        public void InitializePeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeopleBornAfter(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeopleByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person? GetPersonById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetAllPeople()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
