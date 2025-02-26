a8b946e9aaace829eaa4be1c757ba9e2b1e4baab
donp8on <paytonadonis@gmail.com>
2025-02-25 21:21:08 -0500

    feat: implemented InitializePeople data method

--- PaySupplement_P1_8/GetPeople.cs
+++ PaySupplement_P1_8/GetPeople.cs
@@ -6,7 +6,13 @@
 
         public void InitializePeople()
         {
-            throw new NotImplementedException();
+            Random random = new Random();
+            data = Enumerable.Range(1, 1_000_000).Select(id => new Person
+            {
+                Id = id,
+                Name = "Person" + random.Next(1, 10000),  // Random name
+                Birthday = new DateTime(random.Next(1950, 2024), random.Next(1, 13), random.Next(1, 29))  // Random birthday
+            }).ToList();
         }
 
         public IEnumerable<Person> GetPeopleBornAfter(DateTime date)
