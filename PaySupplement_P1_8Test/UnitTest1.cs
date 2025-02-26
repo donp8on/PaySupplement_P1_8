511c030d3afd5853d13a72a27a72101bf1a71509
donp8on <paytonadonis@gmail.com>
2025-02-25 22:08:28 -0500

    test: created test for GetPersonById method

--- PaySupplement_P1_8Test/UnitTest1.cs
+++ PaySupplement_P1_8Test/UnitTest1.cs
@@ -33,5 +33,19 @@ namespace PaySupplement_P1_8Test
             Assert.NotEmpty(results); 
             Assert.All(results, p => Assert.Equal(randomPerson.Name, p.Name));
         }
+
+        [Fact]
+        public void ShouldGetPersonById_ReturnCorrectPerson()
+        {
+            var allPeople = _getPeople.GetAllPeople();
+            var randomPerson = allPeople.First();
+
+            var foundPerson = _getPeople.GetPersonById(randomPerson.Id);
+
+            Assert.NotNull(foundPerson);
+            Assert.Equal(randomPerson.Id, foundPerson.Id);
+            Assert.Equal(randomPerson.Name, foundPerson.Name);
+            Assert.Equal(randomPerson.Birthday, foundPerson.Birthday);
+        }
     }
 }
