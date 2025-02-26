5f4227a40b351d61f0727557bf40d0979bee08b3
donp8on <paytonadonis@gmail.com>
2025-02-25 21:22:53 -0500

    test: declared initial getPeople test

--- PaySupplement_P1_8Test/UnitTest1.cs
+++ PaySupplement_P1_8Test/UnitTest1.cs
@@ -1,11 +1,15 @@
+using PaySupplement_P1_8;
+
 namespace PaySupplement_P1_8Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly GetPeople _getPeople;
 
+        public UnitTest1()
+        {
+            _getPeople = new GetPeople();
+            _getPeople.InitializePeople();
         }
     }
 }
